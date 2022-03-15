626d3094ddf6d964955de57f80c43387f3dcfb56
Tomáš Kalvoda <tomas.kalvoda@fit.cvut.cz>
2022-03-15 22:34:08 +0100

    Add default tikz & mathjax configs.

--- src/lib/wootom.ts
+++ src/lib/wootom.ts
@@ -68,7 +68,51 @@ export const config = {
             ' is normally given to the MathJax (v2.x) config as the `Macros`' +
             ' option. Might require an editor reload to take effect.',
         type: 'string',
-        default: '{}',
+        default: '{"N": "\\\\mathbb{N}",\n' +  // Keep this in sync with
+                 '"R": "\\\\mathbb{R}",\n' + // fit-html & fit-pdf templates!
+                 '"Z": "\\\\mathbb{Z}",\n' +
+                 '"Q": "\\\\mathbb{Q}",\n' +
+                 '"R": "\\\\mathbb{R}",\n' +
+                 '"CC": "\\\\mathbb{C}",\n' +
+                 '"e": "\\\\mathrm{e}",\n' +
+                 '"ii": "\\\\mathrm{i}",\n' +
+                 '"Re": "\\\\mathrm{Re}\\\\,",\n' +
+                 '"Im": "\\\\mathrm{Im}\\\\,",\n' +
+                 '"tg": "\\\\mathrm{tg}",\n' +
+                 '"ctg": "\\\\mathrm{cotg}",\n' +
+                 '"cotg": "\\\\mathrm{cotg}",\n' +
+                 '"arctg": "\\\\mathrm{arctg}",\n' +
+                 '"arcctg": "\\\\mathrm{arccotg}",\n' +
+                 '"ceq": ":=",\n' +
+                 '"veps": "\\\\varepsilon",\n' +
+                 '"sgn": "\\\\mathrm{sgn}",\n' +
+                 '"dt": "\\\\,\\\\mathrm{d}t",\n' +
+                 '"dx": "\\\\,\\\\mathrm{d}x",\n' +
+                 '"mA": "\\\\mathbf{A}",\n' +
+                 '"mB": "\\\\mathbf{B}",\n' +
+                 '"mC": "\\\\mathbf{C}",\n' +
+                 '"mD": "\\\\mathbf{D}",\n' +
+                 '"mE": "\\\\mathbf{E}",\n' +
+                 '"mP": "\\\\mathbf{P}",\n' +
+                 '"mQ": "\\\\mathbf{Q}",\n' +
+                 '"mX": "\\\\mathbf{X}",\n' +
+                 '"vx": "\\\\mathbf{x}",\n' +
+                 '"vy": "\\\\mathbf{y}",\n' +
+                 '"vz": "\\\\mathbf{z}",\n' +
+                 '"va": "\\\\mathbf{a}",\n' +
+                 '"vb": "\\\\mathbf{b}",\n' +
+                 '"vc": "\\\\mathbf{c}",\n' +
+                 '"ve": "\\\\mathbf{e}",\n' +
+                 '"vu": "\\\\mathbf{u}",\n' +
+                 '"vv": "\\\\mathbf{v}",\n' +
+                 '"vw": "\\\\mathbf{w}",\n' +
+                 '"XX": "\\\\mathcal{X}",\n' +
+                 '"YY": "\\\\mathcal{Y}",\n' +
+                 '"EE": "\\\\mathcal{E}",\n' +
+                 '"ssubset": "\\\\subset\\\\subset",\n' +
+                 '"pmat": [ "\\\\left(\\\\!\\\\!\\\\begin{array}{#1}#2\\\\end{array}\\\\!\\\\!\\\\right)", 2 ],\n' +
+                 '"logi": "\\\\mkern4mu{\\\\Large\\\\shortmid}\\\\mkern-8mu\\\\raise{0.5pt}=\\\\mkern-8mu{\\\\Large\\\\shortmid}\\\\mkern4mu",\n' +
+                 '"thus": "\\\\mkern4mu{\\\\Large\\\\shortmid}\\\\mkern-8mu\\\\raise{0.5pt}=\\\\mkern-8mu\\\\mkern8mu"}'
     },
     tikzPreamble: {
         title: 'TikZ Preamble',
@@ -78,7 +122,100 @@ export const config = {
             ' Note that `\\documentclass[tikz]{standalone}` is always' +
             ' automatically included in the preamble.',
         type: 'string',
-        default: '',
+        default: '\\usepackage[utf8]{inputenc}\n' +
+                 '\\usepackage[T1]{fontenc}\n' +
+                 '\\usepackage[czech]{babel}\n' +
+                 '\\usepackage{libertine}\n' +
+                 '\\usepackage{amsmath}\n' +
+                 '\\usepackage{amssymb}\n' +
+                 '\\usepackage{tikz}\n' +
+                 '\\usepackage{ifthen}\n' +
+                 '\\usepackage{pgfplots}\n' +
+                 '\\usepackage{bbding}\n' +
+                 '\\newcommand{\\N}{\\mathbb{N}}\n' +
+                 '\\newcommand{\\Z}{\\mathbb{Z}}\n' +
+                 '\\newcommand{\\Q}{\\mathbb{Q}}\n' +
+                 '\\newcommand{\\R}{\\mathbb{R}}\n' +
+                 '\\newcommand{\\CC}{\\mathbb{C}}\n' +
+                 '\\newcommand{\\e}{\\mathrm{e}}\n' +
+                 '\\newcommand{\\ii}{\\mathrm{i}}\n' +
+                 '\\renewcommand{\\Re}{\\mathrm{Re}\\,}\n' +
+                 '\\renewcommand{\\Im}{\\mathrm{Im}\\,}\n' +
+                 '\\DeclareMathOperator{\\tg}{\\mathrm{tg}}\n' +
+                 '\\DeclareMathOperator{\\ctg}{\\mathrm{cotg}}\n' +
+                 '\\DeclareMathOperator{\\cotg}{\\mathrm{cotg}}\n' +
+                 '\\DeclareMathOperator{\\arctg}{\\mathrm{arctg}}\n' +
+                 '\\DeclareMathOperator{\\arcctg}{\\mathrm{arccotg}}\n' +
+                 '\\newcommand{\\ceq}{\\mathrel{\\mathop:}=}\n' +
+                 '\\newcommand{\\veps}{\\varepsilon}\n' +
+                 '\\DeclareMathOperator{\\sgn}{\\mathrm{sgn}}\n' +
+                 '\\newcommand{\\dx}{\\,\\mathrm{d}x}\n' +
+                 '\\newcommand{\\dt}{\\,\\mathrm{d}t}\n' +
+                 '% matrices and vectors\n' +
+                 '\\newcommand{\\mA}{\\mathbf{A}}\n' +
+                 '\\newcommand{\\mB}{\\mathbf{B}}\n' +
+                 '\\newcommand{\\mC}{\\mathbf{C}}\n' +
+                 '\\newcommand{\\mD}{\\mathbf{D}}\n' +
+                 '\\newcommand{\\mE}{\\mathbf{E}}\n' +
+                 '\\newcommand{\\mP}{\\mathbf{P}}\n' +
+                 '\\newcommand{\\mQ}{\\mathbf{Q}}\n' +
+                 '\\newcommand{\\mX}{\\mathbf{X}}\n' +
+                 '\\newcommand{\\vx}{\\mathbf{x}}\n' +
+                 '\\newcommand{\\vy}{\\mathbf{y}}\n' +
+                 '\\newcommand{\\vz}{\\mathbf{z}}\n' +
+                 '\\newcommand{\\va}{\\mathbf{a}}\n' +
+                 '\\newcommand{\\vb}{\\mathbf{b}}\n' +
+                 '\\newcommand{\\vc}{\\mathbf{c}}\n' +
+                 '\\newcommand{\\ve}{\\mathbf{e}}\n' +
+                 '\\newcommand{\\vu}{\\mathbf{u}}\n' +
+                 '\\newcommand{\\vv}{\\mathbf{v}}\n' +
+                 '\\newcommand{\\vw}{\\mathbf{w}}\n' +
+                 '\\newcommand{\\XX}{\\mathcal{X}}\n' +
+                 '\\newcommand{\\YY}{\\mathcal{Y}}\n' +
+                 '\\newcommand{\\EE}{\\mathcal{E}}\n' +
+                 '\\newcommand{\\ssubset}{\\subset\\subset}\n' +
+                 '% matrices with vertical lines\n' +
+                 '\\newcommand{\\pmat}[2]{\\left(\\!\\!\\begin{array}{#1}#2\\end{array}\\!\\!\\right)}\n' +
+                 '\\usetikzlibrary{\n' +
+                 '  shapes.geometric,\n' +
+                 '  calc,\n' +
+                 '  arrows,\n' +
+                 '  decorations.markings,\n' +
+                 '  decorations.pathmorphing\n' +
+                 '}\n' +
+                 '\\pgfplotsset{\n' +
+                 '  % all plots are thick and without point marks\n' +
+                 '  every axis plot/.append style={\n' +
+                 '    thick,\n' +
+                 '    mark=none,\n' +
+                 '    smooth\n' +
+                 '  },\n' +
+                 '  % axis style\n' +
+                 '  every axis/.append style={\n' +
+                 '    axis lines=middle,\n' +
+                 '    axis line style={->,thick},\n' +
+                 '    grid=major,\n' +
+                 '    minor tick num=1,\n' +
+                 '    tick label style={font=\\footnotesize,fill=white,opacity=0.5,text opacity=1.0}\n' +
+                 '  },\n' +
+                 '  % grid style\n' +
+                 '  every axis grid/.style={\n' +
+                 '    thin,\n' +
+                 '    gray,\n' +
+                 '    dashed\n' +
+                 '  },\n' +
+                 '  % legend style\n' +
+                 '  every axis legend/.append style={\n' +
+                 '    at={(1.1,0.5)},\n' +
+                 '    anchor=west,\n' +
+                 '    %legend pos=outer east,\n' +
+                 '    %legend columns=2,\n' +
+                 '    cells={anchor=west},\n' +
+                 '    font=\\footnotesize,\n' +
+                 '    rounded corners=2pt\n' +
+                 '  },\n' +
+                 '  xlabel={$x$},ylabel={$y$}\n' +
+                 '}',
     },
     tikzSvgStyle: {
         title: 'TikZ SVG Style',
